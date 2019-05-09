4b544de004a5d98151197315f1eefcd404ee5983
Na2CO3ren <zebin_ren@163.com>
2019-05-09 19:22:37 +0800

    add success page by thymeleaf

--- src/main/java/robin/springboot/restfulcrud/comtroller/HelloController.java
+++ src/main/java/robin/springboot/restfulcrud/comtroller/HelloController.java
@@ -4,6 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.jws.Oneway;
+import java.util.Arrays;
+import java.util.Map;
+
 @Controller
 public class HelloController {
 
@@ -11,5 +15,12 @@ public class HelloController {
     @RequestMapping("/hello")
     public String hello() {
         return "Hello World";
+    }
+
+    @RequestMapping("/testConnection")
+    public String testConnection(Map<String, Object> map) {
+        map.put("hello","你好");
+        map.put("persons", Arrays.asList("robin","jinglan","yiyi"));
+        return "success";
     }
 }
